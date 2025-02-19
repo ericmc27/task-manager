4e48183650de36064d2bd5edf5aa1fcdfeb14fac
unknown <erictomas27@hotmail.com>
2025-02-19 13:39:06 -0500

    initial config

---
+++ pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = ()=>{
+  return(
+    <div className="bg-red-600">
+      Login route
+    </div>
+  )
+}
+
+export default Login
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Login from "../pages/Login"
+
+const router = createBrowserRouter([
+  {
+    path:'/login',
+    element: <Login/>
+  }
+])
+
+
+function App() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
